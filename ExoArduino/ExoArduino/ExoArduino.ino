7c4ab8b61a51fb6a8988e47f4c26d9cdfb709fa8
Hao <haolee0702@engineering.ucla.edu>
2018-10-01 19:32:35 -0700

    fix arduino data length

--- ExoArduino/ExoArduino/ExoArduino.ino
+++ ExoArduino/ExoArduino/ExoArduino.ino
@@ -2,6 +2,7 @@
  Name:		ExoArduino.ino
  Author:	Hao Lee
 */
+#include <string.h>
 const int NUMSAMP = 3;
 const int NUMSEN = 9;
 const int SAMPINTERVAL = 5; //unit is ms
@@ -12,9 +13,49 @@ int curIndex;
 int senData[NUMSEN][NUMSAMP];
 int senSum[NUMSEN];
 String sendResult;
+String Z1 = String("0");
+String Z2 = String("00");
+String Z3 = String("000");
+String Z4 = String("0000");
+String Z5 = String("00000");
+String Z6 = String("000000");
+String Z7 = String("0000000");
+String addZero(String curIn, unsigned int digit)
+{
+	String result=String("");
+	switch ((digit-curIn.length()))
+	{
+	case 0:
+		result= curIn;
+		break;
+	case 1:
+		result = Z1 + curIn;
+		break;
+	case 2:
+		result= Z2 + curIn;
+		break;
+	case 3:
+		result = Z3 + curIn;
+		break;
+	case 4:
+		result = Z4 + curIn;
+		break;
+	case 5:
+		result= Z5 + curIn;
+		break;
+	case 6:
+		result = Z6 + curIn;
+		break;
+	case 7:
+		result = Z7 + curIn;
+	default:
+		break;
+	}
+	return result;
+}
 // the setup function runs once when you press reset or power the board
 void setup() {
-	Serial.begin(115200);
+	Serial.begin(115200,SERIAL_8E1);
 	for (int i = 0; i < NUMSEN; i++) {
 		for (int k = 0; k < NUMSAMP; k++)
 			senData[i][k] = 0;
@@ -27,17 +68,18 @@ void setup() {
 void loop() {
 	sendResult = "@";
 	unsigned long curTime = millis()%MAXTIME;
-	sendResult += String(curTime);
+	sendResult += addZero(String(curTime),7);
 	
 	for (int senIndex = 0; senIndex < NUMSEN; senIndex++) {
 		sendResult += ",";
 		senSum[senIndex] = senSum[senIndex] - senData[senIndex][curIndex];
 		senData[senIndex][curIndex] = analogRead(sensorArray[senIndex]);
 		senSum[senIndex] = senSum[senIndex] + senData[senIndex][curIndex];
-		sendResult += String((double)senSum[senIndex] / NUMSAMP, 0);
+		sendResult += addZero(String((double)senSum[senIndex] / NUMSAMP, 0),3);
 	}
 	sendResult = sendResult + "\n";
-	//sendResult = "@0,1,2,3,4,5,6,7,8,9\n";
+	//sendResult = "@11111,222,333,444,555,666,777,888,999,123\n";
+	//sendResult = Z1+ sendResult;
 	Serial.print(sendResult);
 	curIndex++;
 	if (curIndex == NUMSAMP) 
