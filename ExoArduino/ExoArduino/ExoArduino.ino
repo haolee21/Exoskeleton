d7f14e9808c3d1b9e32a5b608c7a2811af88d570
Hao Lee <haolee0702@engineering.ucla.edu>
2019-06-18 12:04:45 -0700

    (arduino)read in 500 hz, each measurement is 4 bytes.

--- ExoArduino/ExoArduino/ExoArduino.ino
+++ ExoArduino/ExoArduino/ExoArduino.ino
@@ -3,15 +3,14 @@
  Author:	Hao Lee
 */
 
-
 //This is for the test pin
-bool pinCond = false;
+bool pinCond = true;
 //
 
-const int NUMSAMP = 4; 
+const int NUMSAMP = 4;
 const int NUMSEN = 9;
-const int SAMPDIV = 2;//This actually take 4 samples, use measurement>>2 to divide by 4
-int sensorArray[] = {0,1,2,3,4,5,6,A15,A10};
+const int SAMPDIV = 2; //This actually take 4 samples, use measurement>>2 to divide by 4
+int sensorArray[] = {0, 1, 2, 3, 4, 5, 6, A15, A10};
 int curIndex;
 
 int senData[NUMSEN][NUMSAMP];
@@ -23,96 +22,99 @@ int curCont = 1;
 bool readyToSend;
 
 // buffer and array for sensing
-union SenDataType
-{
+union SenDataType {
 	int senVal;
 	char senByte[4];
 };
-union TimeDataType
-{
+union TimeDataType {
 	unsigned long timeVal;
 	char timeByte[4];
-
 };
 TimeDataType curTime;
 SenDataType curSen;
 
 // the setup function runs once when you press reset or power the board
-void setup() {
-	curIndex =0;
+void setup()
+{
+	curIndex = 0;
 	bufferPointer = buffer;
-	Serial.begin(115200,SERIAL_8E1);
-	for (int i = 0; i < NUMSEN; i++) {
+	Serial.begin(1000000, SERIAL_8E1);
+	for (int i = 0; i < NUMSEN; i++)
+	{
 		for (int k = 0; k < NUMSAMP; k++)
 			senData[i][k] = 0;
 	}
 	readyToSend = false;
 	//I use this pin to test the frequency
-	pinMode(50,OUTPUT);
-	
+	pinMode(50, OUTPUT);
 
-// TIMER 1 for interrupt frequency 200 Hz:
-cli(); // stop interrupts
-TCCR1A = 0; // set entire TCCR1A register to 0
-TCCR1B = 0; // same for TCCR1B
-TCNT1  = 0; // initialize counter value to 0
-// set compare match register for 200 Hz increments
-OCR1A = 9999; // = 16000000 / (8 * 200) - 1 (must be <65536)
-// turn on CTC mode
-TCCR1B |= (1 << WGM12);
-// Set CS12, CS11 and CS10 bits for 8 prescaler
-TCCR1B |= (0 << CS12) | (1 << CS11) | (0 << CS10);
-// enable timer compare interrupt
-TIMSK1 |= (1 << OCIE1A);
-sei(); // allow interrupts
-
+	// Timer setting: http://www.8bit-era.cz/arduino-timer-interrupts-calculator.html
+	// TIMER 1 for interrupt frequency 500 Hz:
+	cli();		// stop interrupts
+	TCCR1A = 0; // set entire TCCR1A register to 0
+	TCCR1B = 0; // same for TCCR1B
+	TCNT1 = 0;  // initialize counter value to 0
+	// set compare match register for 500 Hz increments
+	OCR1A = 31999; // = 16000000 / (1 * 500) - 1 (must be <65536)
+	// turn on CTC mode
+	TCCR1B |= (1 << WGM12);
+	// Set CS12, CS11 and CS10 bits for 1 prescaler
+	TCCR1B |= (0 << CS12) | (0 << CS11) | (1 << CS10);
+	// enable timer compare interrupt
+	TIMSK1 |= (1 << OCIE1A);
+	sei(); // allow interrupts
 }
 
 // the loop function runs over and over again until power down or reset
-ISR(TIMER1_COMPA_vect){
-	*bufferPointer++ = '@';
-	curTime.timeVal = millis();
-	for (int sendIndex=0;sendIndex<4;sendIndex++){
-		*bufferPointer = curTime.timeByte[sendIndex];
-		//*bufferPointer++ = 'K';
-	}
-	
-	for (int senIndex = 0; senIndex < NUMSEN; senIndex++) {
-		
-		senSum[senIndex] = senSum[senIndex] - senData[senIndex][curIndex];
-		senData[senIndex][curIndex] = analogRead(sensorArray[senIndex]);
-		//senData[senIndex][curIndex] = 121;  //this gives us y
-		senSum[senIndex] = senSum[senIndex] + senData[senIndex][curIndex];
-		curSen.senVal = senSum[senIndex]>>SAMPDIV;
-		for (int sendIndex=0;sendIndex<4;sendIndex++){
-			*bufferPointer++ =  curSen.senByte[sendIndex];
-		}
-	}
-	*bufferPointer++='\n';
-	//Create the output data 
+ISR(TIMER1_COMPA_vect)
+{
 	readyToSend = true;
-	
-	
-	// if(pinCond){
-	// 	digitalWrite(50,HIGH);
-	// 	pinCond = false;
-	// }
-	// else{
-	// 	digitalWrite(50,LOW);
-	// 	pinCond = true;
-	// }
-	
-	curIndex++; //This is index for moving average filter
-	if (curIndex == NUMSAMP) 
-		curIndex = 0;	
-
 }
 void loop()
 {
-	if (readyToSend){
-		Serial.write(buffer,42);
+	if (readyToSend)
+	{
+		*bufferPointer++ = '@';
+		curTime.timeVal = millis();
+		for (int sendIndex = 0; sendIndex < 4; sendIndex++)
+		{
+			*bufferPointer++ = curTime.timeByte[sendIndex];
+			//*bufferPointer++ = 'K';
+		}
+
+		for (int senIndex = 0; senIndex < NUMSEN; senIndex++)
+		{
+
+			senSum[senIndex] = senSum[senIndex] - senData[senIndex][curIndex];
+			senData[senIndex][curIndex] = analogRead(sensorArray[senIndex]);
+			//senData[senIndex][curIndex] = 121;  //this gives us y
+			senSum[senIndex] = senSum[senIndex] + senData[senIndex][curIndex];
+			curSen.senVal = senSum[senIndex] >> SAMPDIV;
+			for (int sendIndex = 0; sendIndex < 4; sendIndex++)
+			{
+				*bufferPointer++ = curSen.senByte[sendIndex];
+			}
+		}
+		*bufferPointer++ = '\n';
+		//Create the output data
+
+		if (pinCond)
+		{
+			digitalWrite(50, HIGH);
+			pinCond = false;
+		}
+		else
+		{
+			digitalWrite(50, LOW);
+			pinCond = true;
+		}
+
+		curIndex++; //This is index for moving average filter
+		if (curIndex == NUMSAMP)
+			curIndex = 0;
+
+		Serial.write(buffer, 42);
 		readyToSend = false;
 		bufferPointer = buffer;
 	}
-
 }
