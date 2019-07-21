50d4ce5732aae33cf8a63869a0f144277cbd2f18
Hao Lee <haolee0702@engineering.ucla.edu>
2019-07-21 16:05:45 -0700

    remove files in build since we will make at remote

--- Exo/ExoMain/ExoMain/Script/build/CMakeFiles/lib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "liblib.pdb"
-  "liblib.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
