8006b1194f93f9bd5a5eb5b2c0d0639d9cebe388
Hao Lee <haolee0702@engineering.ucla.edu>
2019-07-29 12:31:57 -0700

    tracked ExoPC

---
+++ ExoPC/main.py
@@ -0,0 +1,41 @@
+import socket
+import sys
+def main():
+    # print("wait for connection\n")
+    # sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    # #bind the socket to the port
+    # server_address = ('192.168.1.142',5005)
+    # sock.bind(server_address)
+
+    # sock.listen()
+    # while True:
+    #     print(sys.stderr, "wait for a connection")
+    #     connection, client_address = sock.accept()
+
+
+    HOST = '192.168.1.142' # Server IP or Hostname
+    PORT = 12345 # Pick an open Port (1000+ recommended), must match the client sport
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Socket created')
+    #managing error exception
+
+    try:
+	    s.bind((HOST, PORT))
+    except socket.error:
+	    print ('Bind failed ')
+    s.listen(5)
+
+    print ('Socket awaiting messages')
+    conn, addr = s.accept()
+    print ('Connected')
+
+    while True:
+        data = (conn.recv(6)).decode('ascii')
+        print(data)
+        if(data =='end'):
+            print(data=='end')
+            break
+    return 0
+if __name__ == '__main__':
+    main()
+    
