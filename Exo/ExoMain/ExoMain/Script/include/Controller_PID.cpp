75749826b88333fe84ebf0e6f42c9653c8317702
Hao Lee <haolee0702@engineering.ucla.edu>
2020-01-30 14:08:02 -0800

    seperate Controller.cpp

---
+++ Exo/ExoMain/ExoMain/Script/include/Controller_PID.cpp
@@ -0,0 +1,25 @@
+#include "Controller.h"
+// Data pre-process for the PID controller (feedback linearization?)
+//===========================================================================================
+float Controller::SupPreInput(int knePre, int tankPre, int desPre)
+{
+
+    return (float)(desPre - knePre) / (tankPre - knePre);
+}
+float Controller::AnkActInput(int ankPre, int tankPre)
+{
+
+    return (float)(this->ankActPre - ankPre) / (tankPre - ankPre);
+}
+float Controller::AnkPreRecInput(int ankPre, int tankPre)
+{
+    float curMea = (float)(ankPre - this->ankRecPre) / (ankPre - tankPre);
+    std::cout << "ank rec input: " << curMea << std::endl;
+    return curMea;
+}
+float Controller::KnePreRecInput(int knePre, int tankPre)
+{
+    float curMea = (float)(knePre - this->kneRecPre) / (knePre - tankPre);
+    std::cout << "kne rec input: " << curMea << std::endl;
+    return curMea;
+}
